26e3a17b9c2c7c49c9791d37e73c3973b2a659be
Micael Macedo Alves Barbosa <micael.barbosa@suporterenova.onmicrosoft.com>
2024-12-23 16:20:39 -0300

    feat: add project

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FundoAmareloDirective } from './shared/fundo-amarelo.directive';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FundoAmareloDirective
+  ]
+})
+export class AppModule { }
